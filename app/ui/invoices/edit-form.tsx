5d66f7a41d05c5093d4552e44d183308b5ae9814
Roman Melnychenko <roman.melnychenko@inspera.no>
2025-04-26 22:47:21 +0300

    full course

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,8 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from "react";
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,8 +19,13 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState: State = { message: null, errors: {} };
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
+
   return (
-    <form>
+    // <form action={updateInvoiceWithId}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -106,6 +113,13 @@ export default function EditInvoiceForm({
                 </label>
               </div>
             </div>
+          </div>
+          <div aria-live="polite" aria-atomic="true">
+            {state.message &&
+                (<p className="mt-2 text-sm text-red-500">
+                  {state.message}
+                </p>)
+            }
           </div>
         </fieldset>
       </div>
